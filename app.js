32122da71e91b5a4fbd6e4e42ec28b77892efeca
kartikav05 <kartikav2001@gmail.com>
2023-02-03 23:33:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+//
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Start Server
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server on 3000");
+})
+//Form Post
+app.post("/", function (req, res) {
+
+    var name = req.body.name;
+    var email = req.body.email;
+    var ph = req.body.ph;
+   
+
+    var data = {
+
+        members: [{
+
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+
+                FNAME: name,
+                PHONE: ph,
+              
+
+            }
+
+        }]
+
+    }
+    var JsonData = JSON.stringify(data);
+    const url = "https://us9.api.mailchimp.com/3.0/lists/fdd768a3cb";
+    const options = {
+
+        method: "POST",
+        auth: "kartik1:l5f69ca0a82411b640a539555a6c9bf1d-us9"
+    }
+
+
+    const request = https.request(url, options, function (response) {
+
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/ok.html")
+        }
+        else
+        res.sendFile(__dirname+"/failure.html")
+
+        response.on("data", function (data) {
+
+            console.log(JSON.parse(data));
+
+        })
+    })
+
+    //console.log(name, bdate);
+    request.write(JsonData);
+    request.end();
+
+})
+
+//GET
+
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/index.html");
+})
+
+//    5f69ca0a82411b640a539555a6c9bf1d-us9   fdd768a3cb
+
